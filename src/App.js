5f6943934d7740dbf7742bc6696d9b64924d7e4e
Irene-24 <okaforirenen@gmail.com>
2020-12-02 19:15:20 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,34 +1,80 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+import
+{
+
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./components/GlobalStyles";
+import { lightTheme, darkTheme } from "./components/Themes";
+import { useDarkMode } from "./components/useDarkMode";
 import dummyData from "./data";
 import CardList from "./components/CardList";
 
-const App = () => {
-  const [videos, setVideos] = useState([]);
+import Page from "./components/Page2";
 
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setVideos(dummyData);
-    }, 1000);
-    return () => clearTimeout(timer);
-  }, []);
 
+
+import "./App.css";
+
+const App = () =>
+{
+  const [ videos, setVideos ] = useState( [] );
+  const [ theme, themeToggler, mountedComponent ] = useDarkMode();
+
+  const themeMode = theme === 'light' ? lightTheme : darkTheme;
+
+  useEffect( () =>
+  {
+    const timer = setTimeout( () =>
+    {
+      setVideos( dummyData );
+    }, 1000 );
+    return () => clearTimeout( timer );
+  }, [] );
+
+  if ( !mountedComponent ) return <div />;
   return (
+    <ThemeProvider theme={ themeMode }>
       <>
-        <div className="App">      
-          {
-            videos.map((list, index) => {
-              return (
-                <section key={index}>
-                  <h2 className="section-title">{list.section}</h2>
-                  <CardList list={list} />
-                  <hr />
-                </section>
-              );
-            })}
-        </div>
+        <GlobalStyles />
+        <Link to="/">
+          Home
+        </Link>
+        <br />
+        <Link to="/test">
+          Test
+        </Link>
+        <br />
+        <Link to="/sub">
+          Other
+        </Link>
+        <Switch>
+          <Route exact path="/">
+            <div className="App">
+              <button onClick={ themeToggler }>Switch Theme</button>
+              {
+                videos.map( ( list, index ) =>
+                {
+                  return (
+                    <section key={ index }>
+                      <h2 className="section-title">{ list.section }</h2>
+                      <CardList list={ list } />
+                      <hr />
+                    </section>
+                  );
+                } ) }
+            </div>
+
+          </Route>
+
+          <Route exact path="/test" component={ Page } />
+        </Switch>
       </>
-    
+    </ThemeProvider>
+
   );
 };
 
---
+++ src/components/Page2.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Page2 ()
+{
+    return (
+        <div>
+            <h1>Main</h1>
+
+            <p> lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem </p>
+        </div>
+    );
+}
+
+export default Page2;
